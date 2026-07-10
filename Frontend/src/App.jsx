a7abba08127ac470679de81c423b17a907ce90ee
Sahil-Raj75 <sraj90015@gmail.com>
2026-07-11 00:58:09 +0530

    Provider updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { RouterProvider } from "react-router";
 import { router } from './routes/app.router';
 import { AuthProvider } from './features/auth/auth.context';
-
+import { InterviewProvider } from './features/interview/interview.context';
 const App = () => {
   return (
-    <AuthProvider>
-      <RouterProvider router={router}/>
-    </AuthProvider>
+    <InterviewProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </InterviewProvider>
   )
 }
 
