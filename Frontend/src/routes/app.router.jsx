1e66abe64283fb70101b19eb1a68df5ee2e01d2c
Sahil-Raj75 <sraj90015@gmail.com>
2026-07-26 17:05:09 +0530

    route update

--- Frontend/src/routes/app.router.jsx
+++ Frontend/src/routes/app.router.jsx
@@ -20,7 +20,7 @@ export const router = createBrowserRouter([
         element: <Protected><Home /></Protected>
     },
     {
-        path: '/interview',
+        path: '/interview/:interviewId',    
         element: <Protected><Report /></Protected>
     }
 ])
